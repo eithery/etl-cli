98fce723a22dde98110bb30e346bb5f6259d45ef
eithery <eithery.pro@gmail.com>
2023-06-19 22:39:48 -0400

    Added ReadTheDocs configuration

--- doc/sphinx/source/conf.py
+++ doc/sphinx/source/conf.py
@@ -1,7 +1,8 @@
+import os
 from importlib import metadata
 
 project = 'etl-toolkit'
-copyright = '2023, eithery'
+copyright = '2023, Eithery'
 author = 'eithery'
 PACKAGE_VERSION = metadata.version('etl-toolkit')
 version = PACKAGE_VERSION
@@ -17,3 +18,27 @@ exclude_patterns = []
 
 html_theme = 'alabaster'
 html_static_path = ['static']
+
+
+if os.environ.get('READTHEDOCS') == 'True':
+    from pathlib import Path
+
+    PROJECT_ROOT = Path(__file__).parent.parent.parent.parent
+    PACKAGE_ROOT = PROJECT_ROOT.joinpath('src', 'etl')
+
+    def run_apidoc(_):
+        from sphinx.ext import apidoc
+
+        apidoc.main([
+            '--force',
+            '--implicit-namespaces',
+            '--module-first',
+            '--separate',
+            '-o',
+            str(PROJECT_ROOT.joinpath('doc', 'sphinx', 'source', 'reference')),
+            str(PACKAGE_ROOT)
+        ])
+
+
+    def setup(app):
+        app.connect('builder-inited', run_apidoc)
--- doc/sphinx/source/index.rst
+++ doc/sphinx/source/index.rst
@@ -7,7 +7,7 @@ ETL Toolkit documentation!
 ==========================
 
 .. toctree::
-   :maxdepth: 3
+   :maxdepth: 2
    :caption: Contents:
 
    reference/modules
